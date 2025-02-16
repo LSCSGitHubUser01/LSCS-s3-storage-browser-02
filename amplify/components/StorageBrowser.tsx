0ac532abe89307abf0aa8d023bd8efffc867841b
Louis Scozzari <lscsmbp01@LSCSMBP-01.local>
2025-02-16 14:11:02 -0600

    remove theme.ts

--- amplify/components/StorageBrowser.tsx
+++ amplify/components/StorageBrowser.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createAmplifyAuthAdapter, createStorageBrowser } from '@aws-amplify/ui-react-storage/browser';
-import { ThemeProvider } from "@aws-amplify/ui-react";
+//import { ThemeProvider } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react-storage/styles.css';
 import { Amplify } from 'aws-amplify';
 //import config from '../../amplify_outputs.json';
